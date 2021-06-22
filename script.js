d83650463b4303a441289fa839af5a5de30ad3fa
Adrián Rejas <adrianrejas@gmail.com>
2021-06-22 19:13:02 +0000

    Changes made according to project requests

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+// Function for working out the interest to be received from user inputs
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        alert("Input a positive quantity in the amount ");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit \<b\>"+principal+"\<\/b\>,\<br\>at an interest rate of \<b\>"+rate+"%\<\/b\>\<br\>You will receive an amount of \<b\>"+interest+"\<\/b\>,\<br\>in the year \<b\>"+year+"\<\/b\>\<br\>"
 }
         
+// Function for updating interest range value
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + "%";
+}
